59bfc3387ae3059ff398220bf4b9858ffc42e4cd
meloman1001 <58601223+meloman1001@users.noreply.github.com>
2020-12-20 20:27:30 +0300

    Initial dist subtree commit

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+export default {
+   target: 'static',
+   mode: 'universal',
+
+   head: {
+      title: 'test1',
+      meta: [
+         {charset: 'utf-8'},
+         {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+         {hid: 'description', name: 'description', content: ''}
+      ],
+      link: [
+         {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+      ]
+   },
+
+   css: [],
+
+   plugins: [],
+
+   components: true,
+
+   buildModules: [],
+
+   modules: [],
+
+   build: {
+   },
+   router: {
+      // base: './'
+   },
+
+}
